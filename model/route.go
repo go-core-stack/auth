0e832b9d5716697c4c9f9cafd69c33e661726778
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-06-03 16:02:01 +0530

    Provide structs to be used in auto generated code (#12)
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

---
+++ model/route.go
@@ -0,0 +1,11 @@
+// Copyright © 2025 Prabhjot Singh Sethi, All Rights reserved
+// Author: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
+
+package model
+
+import "github.com/go-core-stack/auth/route"
+
+type Route struct {
+	Url    string
+	Method route.MethodType
+}
