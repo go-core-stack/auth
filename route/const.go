cacfaa2e7a470781e8cd1e58ec7cf78fa771a49b
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-06-03 07:41:06 +0000

    Add route table option to auth package
    This will keep the routes in a common services database
    which is meant to be used for internal purpose only
    
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

---
+++ route/const.go
@@ -0,0 +1,14 @@
+// Copyright © 2025 Prabhjot Singh Sethi, All Rights reserved
+// Author: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
+
+package route
+
+const (
+	// internal services database name
+	ServicesDatabaseName = "services"
+)
+
+const (
+	// Routes collection name
+	RoutesCollectionName = "routes"
+)
