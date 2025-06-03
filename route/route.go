2ffaba108c4ff0843bd3371ee7014a5f01ae291c
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2025-06-03 13:12:42 +0530

    Add route table option to auth package (#9)
    This will keep the routes in a common services database
    which is meant to be used for internal purpose only
    
    Signed-off-by: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>

---
+++ route/route.go
@@ -0,0 +1,69 @@
+// Copyright © 2025 Prabhjot Singh Sethi, All Rights reserved
+// Author: Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
+
+package route
+
+import (
+	"github.com/go-core-stack/core/db"
+	"github.com/go-core-stack/core/errors"
+	"github.com/go-core-stack/core/table"
+)
+
+type MethodType int32
+
+const (
+	GET MethodType = iota
+	HEAD
+	POST
+	PUT
+	PATCH
+	DELETE
+	CONNECT
+	OPTIONS
+	TRACE
+)
+
+type Key struct {
+	Url    string     `json:"url,omitempty"`
+	Method MethodType `json:"method,omitempty"`
+}
+
+type Route struct {
+	Key      *Key   `json:"key,omitempty"`
+	Endpoint string `json:"endpoint,omitempty"`
+	IsPublic *bool  `json:"isPublic,omitempty"`
+}
+
+type RouteTable struct {
+	table.Table[Key, Route]
+	col db.StoreCollection
+}
+
+var routeTable *RouteTable
+
+func GetTenantTable() (*RouteTable, error) {
+	if routeTable != nil {
+		return routeTable, nil
+	}
+
+	return nil, errors.Wrapf(errors.NotFound, "route table not found")
+}
+
+func LocateTenantTable(client db.StoreClient) (*RouteTable, error) {
+	if routeTable != nil {
+		return routeTable, nil
+	}
+
+	col := client.GetCollection(ServicesDatabaseName, RoutesCollectionName)
+	tbl := &RouteTable{
+		col: col,
+	}
+
+	err := tbl.Initialize(col)
+	if err != nil {
+		return nil, err
+	}
+	routeTable = tbl
+
+	return routeTable, nil
+}
